c370cbfeb0d343d7b1f8c39623e03b669581abda
minhnguyen <minh.nguyen@ilotusland.com>
2023-08-21 10:54:13 +0700

    update 2

--- App.tsx
+++ App.tsx
@@ -1,64 +1,58 @@
-import {StatusBar, StyleSheet, Text, TextInput, View} from 'react-native';
-import React, {FC, ReactNode} from 'react';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import {useResizeMode} from 'react-native-keyboard-controller';
+
 import KeyboardAwareScrollView from './KeyboardAwareScrollView';
-
-const styles = StyleSheet.create({
-  centered: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  container: {
-    flex: 1,
-  },
-  header: {
-    backgroundColor: 'lightblue',
-    height: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  contentContainer: {
-    backgroundColor: 'white',
-    flexGrow: 1,
-  },
-  flex1: {flex: 1},
-  textInput: {
-    width: '100%',
-    height: 50,
-    marginTop: 50,
-    backgroundColor: 'lightgray',
-  },
-});
-
-const Centered: FC<{children: ReactNode}> = ({children}) => (
-  <View style={styles.centered}>{children}</View>
-);
 
 export default function AwareScrollView() {
   useResizeMode();
 
-  // header background style works together follow some states/ changing image background or background color
+  const [manyTextInputs, setMany] = useState<number>(5);
 
   return (
-    <View style={styles.flex1}>
-      <StatusBar barStyle={'dark-content'} backgroundColor={'transparent'} />
-
+    <View style={styles.flex}>
       <View style={styles.header}>
-        <Text>{'Screen header'}</Text>
-        <Text>{'Screen header'}</Text>
-        <Text>{'Screen header'}</Text>
+        <Text>{'number of TextInputs'}</Text>
+        <TextInput
+          value={String(manyTextInputs)}
+          keyboardType="numeric"
+          onChangeText={num => {
+            setMany(Number(num));
+          }}
+          style={{borderWidth: 1}}
+        />
       </View>
-
       <KeyboardAwareScrollView
-        contentContainerStyle={styles.contentContainer}
-        style={styles.container}>
-        <Centered>
-          {new Array(5).fill(0).map((_, i) => (
-            <TextInput key={i} value={i + '_'} style={[styles.textInput]} />
+        style={{flex: 1}}
+        contentContainerStyle={{flexGrow: 1}}>
+        <View style={styles.flexCenter}>
+          {new Array(manyTextInputs).fill(0).map((_, i) => (
+            <TextInput
+              key={i}
+              placeholder={`${i}`}
+              placeholderTextColor="black"
+              style={styles.baseTextInput}
+            />
           ))}
-        </Centered>
+        </View>
       </KeyboardAwareScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  baseTextInput: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'lightgray',
+    marginTop: 50,
+  },
+  flex: {flex: 1},
+  flexCenter: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  header: {
+    borderWidth: 1,
+    backgroundColor: 'lightblue',
+    padding: 20,
+    paddingTop: 46,
+  },
+});
